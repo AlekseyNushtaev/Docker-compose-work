34db692ab2557c045c2520038bdcd3305d694b73
Aleksei Nushtaev <nushtaev2110@gmail.com>
2024-01-25 16:29:01 +0500

    Initial commit

---
+++ code/logistic/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+
+from logistic.models import Product, Stock, StockProduct
+
+admin.site.register(Product)
+admin.site.register(Stock)
+admin.site.register(StockProduct)
+
+# Register your models here.
